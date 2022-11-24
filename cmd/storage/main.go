43205b1a7f9dbd254d6234743fda89d233b15df6
robertgarayshin <robgar@bk.ru>
2022-11-24 15:20:44 +0300

    Initial commit

---
+++ cmd/storage/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+import "storage/internal/storage"
+
+func main() {
+	st := storage.NewStorage()
+	file, err := st.Upload("text.txt", []byte("Hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	restoredFile, err := st.GetByID(file.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("It's restored", restoredFile)
+}
